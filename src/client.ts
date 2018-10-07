089ccbc3dfd98f97e92634a8f02910ce42e78298
Caleb Everett <everett.caleb@gmail.com>
2018-10-06 23:39:25 -0700

    remove async
    parcel is trying to remove async await (typescript is configured to keep
    it, so I think it must be parcel). I'll firegure out why parcel is doing
    this later.

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,16 @@
-async function setLightLevel(level: number) {
+function setLightLevel(level: number) {
   console.log(`set lights to ${level}`);
-  const resp = await fetch('/msg', {
+  fetch('/msg', {
     method: 'POST',
     headers: {
       'x-control': 'lights',
       'x-level': level.toString(),
     },
+  }).then(resp => {
+    if (resp.status < 400) {
+      window.location.href = 'https://www.youtube.com/tv#/watch/video/seek?v=ZZ5LpwO-An4&resumehttps://www.youtube.com/tv#/watch/video/seek?v=ZZ5LpwO-An4&resume';
+    }
   });
-
-  if (resp.status < 400) {
-    window.location.href = 'https://www.youtube.com/tv#/watch/video/seek?v=ZZ5LpwO-An4&resumehttps://www.youtube.com/tv#/watch/video/seek?v=ZZ5LpwO-An4&resume';
-  }
 }
 
 (window as any).setLightLevel = setLightLevel;
