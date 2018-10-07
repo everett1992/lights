59fec0dd334e453ff1a6eed22aa98960654e6704
Caleb Everett <everett.caleb@gmail.com>
2018-10-06 23:32:07 -0700

    Add bad auth

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,16 @@
-function setLightLevel(level: number) {
+async function setLightLevel(level: number) {
   console.log(`set lights to ${level}`);
-  fetch('/msg', {
+  const resp = await fetch('/msg', {
     method: 'POST',
     headers: {
       'x-control': 'lights',
       'x-level': level.toString(),
     },
   });
+
+  if (resp.status < 400) {
+    window.location.href = 'https://www.youtube.com/tv#/watch/video/seek?v=ZZ5LpwO-An4&resumehttps://www.youtube.com/tv#/watch/video/seek?v=ZZ5LpwO-An4&resume';
+  }
 }
 
 (window as any).setLightLevel = setLightLevel;
